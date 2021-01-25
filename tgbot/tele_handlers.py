5b351a241542f33ef6e47cccf4d51bebc72fc284
Nikolai <kirichk@gmail.com>
2021-01-25 16:25:58 +0200

    solution for fix

--- tgbot/tele_handlers.py
+++ tgbot/tele_handlers.py
@@ -322,7 +322,7 @@ def searchfilter_handler(update: Update, context: CallbackContext):
                 cleaned_contract = 6
             else:
                 cleaned_contract = int(re.findall(r'[0-9]+', post['voyage_duration'])[0])
-            if cleaned_sub_salary != '' and cleaned_sub_salary_start <= cleaned_salary >= cleaned_sub_salary_end:
+            if cleaned_sub_salary != '' and cleaned_sub_salary_start <= cleaned_salary and cleaned_salary >= cleaned_sub_salary_end:
                 if cleaned_contract >= cleaned_sub_contract:
                         print(cleaned_sub_salary_start)
                         print(cleaned_sub_salary_end)
@@ -376,7 +376,7 @@ def searchsubscription_handler(update: Update, context: CallbackContext):
                 cleaned_contract = 6
             else:
                 cleaned_contract = int(re.findall(r'[0-9]+', post['voyage_duration'])[0])
-            if cleaned_sub_salary != '' and cleaned_sub_salary_start <= cleaned_salary >= cleaned_sub_salary_end:
+            if cleaned_sub_salary != '' and cleaned_sub_salary_start <= cleaned_salary and cleaned_salary >= cleaned_sub_salary_end:
                 if cleaned_contract >= cleaned_sub_contract:
                     result.append(post)
         page = int(update.callback_query.data.split('#')[1])
