8b91983e284482977dd2be53ad8ca6a71770ce0e
Nikolai <kirichk@gmail.com>
2021-02-06 19:21:34 +0200

    beautified

--- web_hiring/notificators.py
+++ web_hiring/notificators.py
@@ -14,7 +14,8 @@ def vacancy_notification(form):
     Receiving a dictionary with data of new posted vacancy
     Applying filtering to send notification to relevant users
     """
-    title = '#' + form["title"].replace(' ', '_').replace(',', '').replace('.', '',).replace('(','').replace(')', '').replace('-', '_')
+    title = '#' + form["title"].replace(' ', '_').replace(',', '').replace(
+        '.', '',).replace('(', '').replace(')', '').replace('-', '_')
     main_text = f'{form["title"]}\n'\
                 f'Тип судна: {form["vessel"]}\n'\
                 f'Зарплата: {form["salary"]}\n'\
@@ -39,7 +40,7 @@ def vacancy_notification(form):
     for p in Profile.objects.filter(subscription=True):
         if (form['title'] in p.title_subscriptions
             or 'Пропустить' in p.title_subscriptions
-            or '' == p.title_subscriptions):
+                or '' == p.title_subscriptions):
             if p.date_ready is not None and p.date_ready != '':
                 d = datetime.strptime(p.date_ready, '%Y-%m-%d')
             else:
@@ -49,18 +50,20 @@ def vacancy_notification(form):
                     cleaned_sub_salary = 0
                 else:
                     cleaned_sub_salary = int(re.findall(r'[0-9]+',
-                                                p.salary_subscription.split('-')[0])[0])
+                                                        p.salary_subscription.split('-')[0])[0])
                 cleaned_salary = int(re.findall(r'[0-9]+', form['salary'])[0])
                 if p.contract_subscription == '' or p.contract_subscription == 'Не важно' or p.contract_subscription is None:
                     cleaned_sub_contract = 0
                 else:
-                    cleaned_sub_contract = int(re.findall(r'[0-9]+', p.contract_subscription)[0])
+                    cleaned_sub_contract = int(re.findall(
+                        r'[0-9]+', p.contract_subscription)[0])
                 if form['voyage_duration'] is None or form['voyage_duration'] == '':
                     cleaned_contract = 6
                 else:
-                    cleaned_contract = int(re.findall(r'[0-9]+', form['voyage_duration'])[0])
+                    cleaned_contract = int(re.findall(
+                        r'[0-9]+', form['voyage_duration'])[0])
                 if cleaned_salary >= cleaned_sub_salary:
                     if cleaned_contract >= cleaned_sub_contract:
-                        bot.send_message(p.external_id,main_text)
+                        bot.send_message(p.external_id, main_text)
     main_text = main_text.replace(form['title'], title)
     bot.send_message(CHANNEL_ID, main_text)
